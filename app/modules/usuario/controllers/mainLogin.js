8ac0c95af9c0ee29c32300bc20472b58f709921f
bih-goncalves <bih_goncalves@hotmail.com>
2018-02-27 14:11:40 -0300

    inserindo login com user da API

--- app/modules/usuario/controllers/mainLogin.js
+++ app/modules/usuario/controllers/mainLogin.js
@@ -2,8 +2,21 @@
     'use strict';
     angular
       .module('ford.login')
-      .controller('mainLogin', function($scope) {
-  
+      .controller('mainLogin', function($state, $auth) {
+           var vm = this;
+           vm.user = {};
+           vm.login = function __login() {
+             $auth.login({
+               email: vm.user.email,
+               password: vm.user.password
+             }).then(function (response) {
+               console.log(response);
+               $state.go("repositorio");
+             }).catch(function (response) {
+               console.log(response);
+               window.alert("Error: Login failed");
+             });
+           };
       });
   
   })();
--- app/modules/usuario/routes.js
+++ app/modules/usuario/routes.js
@@ -8,9 +8,23 @@
           url: '/login',
           views: {
             '': { 
-              templateUrl: 'modules/usuario/views/login.html' }
+              templateUrl: 'modules/usuario/views/login.html',
+              controller:'mainLogin as login'}
           }
       })
+      .state('register', {
+        url: '/register',
+        views: {
+          '': { 
+         templateUrl: 'modules/usuario/views/register.html',
+         controller:'mainRegister as register' }
+        }
+      });
+      $authProvider.loginUrl = "http://localhost:8000/api/v1/auth/login";
+      $authProvider.signupUrl = "http://localhost:8000/api/v1/auth/register";
+      //inserir url da api de login
+      $authProvider.loginUrl = "http://localhost:8585/api/v1/auth/login";
+      $authProvider.signupUrl = "http://localhost:8585/api/v1/auth/register";
      
   });
 
