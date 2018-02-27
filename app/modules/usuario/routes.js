3c3b917bec78b36814cbdd1ba47b385c15f2c5eb
bih-goncalves <bih_goncalves@hotmail.com>
2018-02-27 14:23:52 -0300

    acertando chamada de biblioteca

--- app/modules/usuario/routes.js
+++ app/modules/usuario/routes.js
@@ -2,7 +2,7 @@
   'use strict';
   angular
     .module('ford.login')
-    .config(function ($stateProvider) {
+    .config(function ($stateProvider, $urlRouterProvider, $authProvider) {
       $stateProvider
         .state('login', {
           url: '/login',
@@ -12,14 +12,14 @@
               controller:'mainLogin as login'}
           }
       })
-      .state('register', {
-        url: '/register',
-        views: {
-          '': { 
-         templateUrl: 'modules/usuario/views/register.html',
-         controller:'mainRegister as register' }
-        }
-      });
+        .state('register', {
+          url: '/register',
+          views: {
+            '': { 
+              templateUrl: 'modules/usuario/views/register.html',
+              controller:'mainRegister as register' }
+          }
+        });
       $authProvider.loginUrl = "http://localhost:8000/api/v1/auth/login";
       $authProvider.signupUrl = "http://localhost:8000/api/v1/auth/register";
       //inserir url da api de login
